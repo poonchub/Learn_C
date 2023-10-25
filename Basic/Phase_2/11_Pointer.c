5212376669cfe6523c56b4a5fd2bee608e89a1dd
poonchub <141898614+poonchub@users.noreply.github.com>
2023-10-25 12:24:14 +0700

    25

--- Basic/Phase_2/11_Pointer.c
+++ Basic/Phase_2/11_Pointer.c
@@ -8,14 +8,19 @@ int main(){
     int *ap = &num;
     char letter = 'A';
     char *bp = &letter;
+    int *num_input;
+
+    printf("Input number: ");
+    scanf("%d",&num_input);     /* การกำหนดค่า ต้องใส่ & */
+    printf("num_input = %d\n\n",num_input);
 
     printf("num = %d\n",num);
     printf("num = %d\n",*ap);
     printf("Address num = %p\n",&num);
-    printf("Address num = %p\n",ap);
+    printf("Address num = %p\n\n",ap);
 
     printf("letter = %c\n",letter);
     printf("letter = %c\n",*bp);
     printf("Address num = %p\n",&letter);
-    printf("Address num = %p\n",bp);
+    printf("Address num = %p\n\n",bp);
 }
--- Basic/Phase_2/12_Function_Pointer.c
+++ Basic/Phase_2/12_Function_Pointer.c
@@ -2,16 +2,26 @@
 ใช้ pointer ในการ return ข้อมูลในฟังก์ชันมากกว่า 1 ตัว
 */
 #include <stdio.h>
-void sum_avg(int, int, int *, float *);
+void sum_avg(int, int, int *, float *);     /* รับ pointer เข้า function */
+void input_num(int *);
+
 int main(){
-    int sump;
+    int sump, num;   /* ไม่ต้องกำหนดตัวแปรเป็น pointer*/
     float avgp;
-    sum_avg(5,4,&sump,&avgp);
+    sum_avg(5,4,&sump,&avgp);   /* ใส่ & เพื่อชี้ให้นำค่าไปเก็บไว้ในตัวแปร เสมือนเป็นตัวแปรที่ใส่ & เป็น pointer */
     printf("Sum = %d\n",sump);
     printf("Average = %.2f\n",avgp);
+    printf("------------------------------\n");
+    input_num(&num);
+    printf("Input number = %d",num);
 }
 
 void sum_avg(int a, int b, int *x, float *y){
     *x = a+b;
     *y = *x/2.0;
+}
+
+void input_num(int *n){
+    printf("Enter a number: ");
+    scanf("%d",n);      /* การกำหนดค่า ไม่ต้องใส่ & */
 }
